7c515cb34c8a02e418a7065cce8534f680c92fcc
Kristoff95 <kristoffer.gonzaga@urios.edu.ph>
2019-12-04 13:48:21 +0800

    UPDATE: design

--- src/components/common/Cmn_button.vue
+++ src/components/common/Cmn_button.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="bg-white w-auto h-10 pl-2 pr-3 flex justify-center items-center cursor-pointer relative" :class="`${this.data.left_boarder ? 'border-l' : ''}`">
-    
+  <div @click="checkAction(data.action, data)" class="bg-white w-auto h-10 pl-2 pr-3 flex justify-center items-center cursor-pointer relative" :class="`${this.data.left_boarder ? 'border-l' : ''}`">
     <i class="text-gray-700 font-medium text-14px" :class="data.icon"></i>
-      <!-- {{data.left_boarder}} -->
-      <!-- {{}} -->
     <span v-if="data.show_name" class="text-14px font-medium ml-2 text-light-grey">{{data.name | capitalize}}</span>
-    <div v-if="data.list.length == 7" class="absolute h-auto w-160px left-0px top-42px flex flex-col shadow-1 rounded bg-white py-8px">
-      <li class="list-none py-4px px-24px text-875 font-normal text-light-grey hover:bg-lighter-grey" v-for="(data, index) in data.list" :key="index">{{data}}</li>
+    <!-- {{typeof data.list}} -->
+    <div v-if="data.list.length > 0 && this.show_dropdown" v-on-clickaway="hide_dropdown"  class="absolute h-auto w-160px left-0px top-42px flex flex-col shadow-1 rounded bg-white py-8px">
+      <li class="list-none py-4px px-24px text-875 font-normal text-light-grey hover:bg-lighter-grey" v-for="(inData, inIndex) in data.list" :key="inIndex">{{inData}}</li>
     </div>
 
 
@@ -17,15 +15,43 @@
 </template>
 
 <script>
+import { mixin as clickaway } from 'vue-clickaway';
+
 export default {
   name: 'cmn_Button',
   props: ["data"],
+  mixins: [ clickaway ],
+  data() {
+    return {
+      show_dropdown: false
+    }
+  },
   filters: {
     capitalize: function (value) {
       if (!value) return ''
       value = value.toString()
       return value.charAt(0).toUpperCase() + value.slice(1)
     }
+  },
+  methods: {
+    checkAction(action, data) {
+      this[`${action}`](data)
+    },
+    dropdown() {
+      this.show_dropdown = !this.show_dropdown
+    },
+    click() {
+      this.$parent.right_slide = true
+      // FIXME - must show right slide 
+    },
+    modal(data) {
+      alert(data.name)
+    },
+
+    // clickaway
+    hide_dropdown() {
+      this.show_dropdown = false
+    },
   }
 }
 </script>
--- src/components/optionsNav/optionsNavC.vue
+++ src/components/optionsNav/optionsNavC.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- body top -->
   <div class="w-auto h-20 bg-light-blue flex items-center justify-between px-4">
+    {{right_slide}}
     <!-- left -->
     <div class="w-full h-full flex items-center">
       <div class="flex h-full items-center justify-between">
@@ -18,6 +19,7 @@
         <div v-for="(data, index) in get_getRightNav" :key="index" class="flex rounded bg-white" :class="`${index > 0 ? 'ml-4' : ''}`">
           <span v-for="(inData, inIndex) in data" :key="inIndex" class="">
             <cmn-button :data="inData"></cmn-button>
+            <!-- FIXME - Add border-radius here when index is zero and last index -->
           </span>
         </div>
       </div>
@@ -26,45 +28,49 @@
 </template>
 
 <script>
+/**
+ * UPDATE: "click" to "action"
+ */
 export default {
   name: 'optionsNavC',
   data() {
     return {
+      right_slide: false,
       left: [ // left nav
         {
           group: [
-            { name: 'back', icon: 'icon-arrow-left', tooltip: 'back', list: [], click: 'click', left_boarder: false, show_name: false },
+            { name: 'back', icon: 'icon-arrow-left', tooltip: 'back', list: [], action: 'click', left_boarder: false, show_name: false },
           ]
         },
         {
           group: [
-            { name: 'desktop', icon: 'icon-desktop', tooltip: 'desktop', list: [], click: 'click', left_boarder: false, show_name: false },
-            { name: 'mobile', icon: 'icon-mobile', tooltip: 'mobile', list: [], click: 'click', left_boarder: true, show_name: false },
+            { name: 'desktop', icon: 'icon-desktop', tooltip: 'desktop', list: [], action: 'click', left_boarder: false, show_name: false },
+            { name: 'mobile', icon: 'icon-mobile', tooltip: 'mobile', list: [], action: 'click', left_boarder: true, show_name: false },
           ]
         },
         {
           group: [
-            { name: 'apps', icon: ' icon-plug', tooltip: 'apps', list: [], click: 'click', left_boarder: false, show_name: false },
-            { name: 'settings', icon: 'icon-setting', tooltip: 'settings', list: ["Integrations", "SEO Meta Data", "Tracking Code", "Custom Code", "Background", "Typography", "General"], click: 'dropdown', left_boarder: true, show_name: true },
-            { name: 'pop up', icon: 'icon-expand', tooltip: 'pop up', list: ["Show Popup", "Edit Settings"], click: 'dropdown', left_boarder: false, show_name: true },
-            { name: 'undo', icon: 'icon-undo', tooltip: 'undo', list: [], click: 'click', left_boarder: true, show_name: false },
-            { name: 'redo', icon: 'icon-redo', tooltip: 'redo', list: [], click: 'click', left_boarder: true, show_name: false },
+            { name: 'apps', icon: ' icon-plug', tooltip: 'apps', list: [], action: 'click', left_boarder: false, show_name: false },
+            { name: 'settings', icon: 'icon-setting', tooltip: 'settings', list: ["Integrations", "SEO Meta Data", "Tracking Code", "Custom Code", "Background", "Typography", "General"], action: 'dropdown', left_boarder: true, show_name: true },
+            { name: 'pop up', icon: 'icon-expand', tooltip: 'pop up', list: ["Show Popup", "Edit Settings"], action: 'dropdown', left_boarder: false, show_name: true },
+            { name: 'undo', icon: 'icon-undo', tooltip: 'undo', list: [], action: 'click', left_boarder: true, show_name: false },
+            { name: 'redo', icon: 'icon-redo', tooltip: 'redo', list: [], action: 'click', left_boarder: true, show_name: false },
           ]
         },
       ],
       right: [ // right nav
         {
           group: [
-            { name: 'sections', icon: 'icon-section', tooltip: 'sections', list: ["Add Section", "Manage"], click: 'dropdown', left_boarder: false, show_name: true },
-            { name: 'rows', icon: 'icon-row', tooltip: 'rows', list: ["Add Row", "Manage"], click: 'dropdown', left_boarder: false, show_name: true },
-            { name: 'columns', icon: 'icon-column', tooltip: 'columns', column_list: [{ list: [{ breadcrubs: 'section > 2 column row' }, { title: 'left column', icon1: 'write', icon2: 'eye' }, { title: 'right column', icon1: 'write', icon2: 'eye' },] }], click: 'show_modal', left_boarder: true, show_name: true },
-            { name: 'elements', icon: 'icon-element', tooltip: 'elements', list: ["Add Element", "Manage"], left_boarder: true, show_name: true }
+            { name: 'sections', icon: 'icon-section', tooltip: 'sections', list: ["Add Section", "Manage"], action: 'dropdown', left_boarder: false, show_name: true },
+            { name: 'rows', icon: 'icon-row', tooltip: 'rows', list: ["Add Row", "Manage"], action: 'dropdown', left_boarder: false, show_name: true },
+            { name: 'columns', icon: 'icon-column', tooltip: 'columns', list: [], column_list: [{ list: [{ breadcrubs: 'section > 2 column row' }, { title: 'left column', icon1: 'write', icon2: 'eye' }, { title: 'right column', icon1: 'write', icon2: 'eye' },] }], action: 'modal', left_boarder: true, show_name: true },
+            { name: 'elements', icon: 'icon-element', tooltip: 'elements', list: ["Add Element", "Manage"], action: 'dropdown', left_boarder: true, show_name: true }
           ]
         },
         {
           group: [
-            { name: 'preview', icon: 'icon-eye', tooltip: 'preview', list: [], click: 'click', left_boarder: false, show_name: true },
-            { name: 'save', icon: 'icon-disk', tooltip: 'save', list: [], click: 'click', left_boarder: true, show_name: true }
+            { name: 'preview', icon: 'icon-eye', tooltip: 'preview', list: [], action: 'click', left_boarder: false, show_name: true },
+            { name: 'save', icon: 'icon-disk', tooltip: 'save', list: [], action: 'click', left_boarder: true, show_name: true }
           ]
         }
       ]
