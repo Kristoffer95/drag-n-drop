c67cc334dd34c4af4019f215070c53e34c0857fe
Kristoff95 <kristoffer.gonzaga@urios.edu.ph>
2019-12-17 04:03:35 +0800

    UPDATED: row - drag and drop

--- src/store/modules/pageData.js
+++ src/store/modules/pageData.js
@@ -3,29 +3,29 @@ import { createSetters } from 'map-state-vmodel'
 // initial state
 const state = {
   // body data "Rows and Columns inside"
-  // wpSections: [
-  //   {
-  //     name: 'section',
-  //     row_list: []
-  //   }
-  // ],
   wpSections: [
     {
-      "name": "section",
-      "row_list": [
-        [
-          {
-            "name": "column",
-            "element_list": []
-          },
-          {
-            "name": "column",
-            "element_list": []
-          }
-        ]
-      ]
+      name: 'section',
+      row_list: []
     }
   ],
+  // wpSections: [
+  //   {
+  //     "name": "section",
+  //     "row_list": [
+  //       [
+  //         {
+  //           "name": "column",
+  //           "element_list": []
+  //         },
+  //         {
+  //           "name": "column",
+  //           "element_list": []
+  //         }
+  //       ]
+  //     ]
+  //   }
+  // ],
 
   // updated when a specific "element" of a "row" in a "section" is clicked.
   clicked_section: 0,
