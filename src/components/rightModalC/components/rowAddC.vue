7480623b1ce30f935e5471e25987ee09575260cf
Kristoff95 <kristoffer.gonzaga@urios.edu.ph>
2019-12-17 04:01:14 +0800

    UPDATED: row drag and drop

--- src/components/rightModalC/components/rowAddC.vue
+++ src/components/rightModalC/components/rowAddC.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-full pt-30px">
     <draggable class="w-full flex flex-wrap justify-center"
-    :value="col_count"
+    :value="drag_drop"
     :sort="false"
     :group="{ name: 'row', pull: 'clone', put: false }"
     >
       <div v-for="(data, index) in col_count" :key="index" class="w-155px h-102px border-2px border-lighter-grey rounded cursor-pointer" 
         :class="[(index % 2 == 0 ? 'mr-10px' : ''), (index > 1 ? 'mt-10px' : '')]"
-        @click="addRow(data)">
+        @click="addRow(data[0])">
         <div class="w-full h-full flex flex-col sjustify-center items-center">
           <i class="icon-column text-35px mt-10px text-light-grey"></i>
           <span class="text-12px font-semibold text-dark-grey">{{ data[0].title | uppercase }}</span>
@@ -35,6 +35,16 @@ export default {
   },
   data(){
     return{
+      drag_drop: [
+        [{ name: 'column', element_list: [] }],
+        [{ name: 'column', element_list: [] }, { name: 'column', element_list: [] }],
+        [{ name: 'column', element_list: [] }, { name: 'column', element_list: [] }, { name: 'column', element_list: [] }],
+        [{ name: 'column', element_list: [] }, { name: 'column', element_list: [] }, { name: 'column', element_list: [] }, { name: 'column', element_list: [] }],
+        [{ name: 'column', element_list: [] }, { name: 'column', element_list: [] }, { name: 'column', element_list: [] }, { name: 'column', element_list: [] }, { name: 'column', element_list: [] }],
+        [{ name: 'column', element_list: [] }, { name: 'column', element_list: [] }, { name: 'column', element_list: [] }, { name: 'column', element_list: [] }, { name: 'column', element_list: [] }, { name: 'column', element_list: [] }],
+        [],
+        [],
+      ],
       col_count: [
         [{ title: '1 column', list:[{ name: 'column', element_list: [] }] }],
         [{ title: '2 column', list:[{ name: 'column', element_list: [] }, { name: 'column', element_list: [] }] }],
@@ -55,12 +65,12 @@ export default {
   },
   methods: {
     addRow(data) {
-      if(this.wpSections.length === 1) {
-      this.wpSections[0].row_list.push(data.list)
-      }else{
+      // if(this.wpSections.length === 1) {
+        this.wpSections[0].row_list.push(data.list)
+      // }else{
         // this.wpSections.push(data)
-        alert(this.wpSections.length)
-      }
+        // alert(this.wpSections.length)
+      // }
       this.show_rightModal = false
     }
   },
